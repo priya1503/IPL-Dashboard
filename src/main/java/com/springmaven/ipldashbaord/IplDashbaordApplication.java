4914d33a60894b8ea95b2b78564554c75881a19e
priya1503 <blpriya1503@gmail.com>
2022-07-05 10:33:26 +0530

    changed port

--- src/main/java/com/springmaven/ipldashbaord/IplDashbaordApplication.java
+++ src/main/java/com/springmaven/ipldashbaord/IplDashbaordApplication.java
@@ -9,9 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IplDashbaordApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication app = new SpringApplication(IplDashbaordApplication.class);
-		//app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
-		//app.run(args);
+		SpringApplication app = new SpringApplication(IplDashbaordApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+		app.run(args);
 		SpringApplication.run(IplDashbaordApplication.class, args);
 	}
 }
